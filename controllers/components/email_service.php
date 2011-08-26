26e983ffdea017a7659e48874e39bdb8c4136979
Lucas Ferreira <panchorf@gmail.com>
2011-08-26 19:58:58 -0300

    adding component file

---
+++ controllers/components/email_service.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ * Email Service Component class for Amazon SES
+ *
+ * Send e-mails with Amazon SES
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * Based on original class from: http://bakery.cakephp.org/articles/dankroad/2011/01/30/integrate_amazon_simple_email_service_ses_into_existing_application
+ *
+ * Usage:
+ * 
+ * Load EmailService in your controller components
+ * var $components = array('EmailService');
+ *
+ * In your action choose aws_ses as delivery option
+ * $this->EmailService->delivery = 'aws_ses'; // or 'aws_ses_raw' if you want complex e-mail with attachments
+ *
+ * @author Lucas Ferreira
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @copyright Copyright 2011, Burn web.studio - http://www.burnweb.com.br/
+ * @version 1.1b
+ */
+
+App::import('Vendor', 'aws-sdk', array('file' => 'sdk.class.php')); 
+App::import('Component', 'Email'); 
+
+class EmailServiceComponent extends EmailComponent { 
+    
+	var $ses_options = array();
+
+	function getAmazonSES()
+	{
+		$ses = new AmazonSES();
+		return $ses;
+	}
+	
+	function _aws_ses_raw()
+	{
+		$ses = $this->getAmazonSES();
+		
+		// remove unnecessary headers...
+		$uhs[] = '0: This part of the E-mail should never be seen. If';
+		$uhs[] = '1: you are reading this, consider upgrading your e-mail';
+		$uhs[] = '2: client to a MIME-compatible client.';
+		foreach($uhs as $uh)
+		{
+			if(($p=array_search($uh, $this->__header)))
+			{
+				unset($this->__header[$p]);
+			}
+		}
+		
+		if(!is_array($this->to)) $this->to = array($this->to);
+		$this->__header[] = 'To: ' . implode(', ', array_map(array($this, '_formatAddress'), $this->to));
+		
+		$this->__header[] = 'Subject: ' . $this->_encode($this->subject);
+		
+		$header = implode("\r\n", $this->__header);
+		$message = implode("\r\n", $this->__message);
+		$data = base64_encode($header . "\r\n\r\n" . $message . "\r\n\r\n\r\n.");
+		
+		$options = array_merge($this->ses_options, array( 
+			'Source' => $this->from 
+		));
+		
+		$response = $ses->send_raw_email(array('Data' => $data), $options); 
+		
+		$ok = $response->isOK(); 
+		if(!$ok)
+		{ 
+			$this->log('Error sending raw email from AWS SES: ' . $response->body->asXML(), 'debug'); 
+		}
+		
+		return $ok;
+	}
+
+	function _aws_ses()
+	{
+		$ses = $this->getAmazonSES();
+		
+		$destination = array( 
+			'ToAddresses' => explode(',', $this->to) 
+		);
+		if($this->cc && !empty($this->cc))
+		{
+			$destination = array_merge($destination, array( 
+				'CcAddresses' => $this->cc
+			));
+		}
+		if($this->bcc && !empty($this->bcc))
+		{
+			$destination = array_merge($destination, array( 
+				'BccAddresses' => $this->bcc
+			));
+		}	
+		
+		$options = $this->ses_options;
+		if($this->replyTo && !empty($this->replyTo))
+		{
+			$options = array_merge($options, array( 
+				'ReplyToAddresses' => explode(',', $this->replyTo)
+			));
+		}
+			
+		$message = array( 
+			'Subject' => array( 
+				'Data' => $this->subject,
+				'Charset' => $this->charset
+			), 
+			'Body' => array() 
+		); 
+		if($this->textMessage != null)
+		{ 
+			$message['Body']['Text'] = array( 
+				'Data' => $this->textMessage,
+				'Charset' => $this->charset 
+			); 
+		} 
+		if($this->htmlMessage != null)
+		{ 
+			$message['Body']['Html'] = array( 
+				'Data' => $this->htmlMessage,
+				'Charset' => $this->charset 
+			); 
+		} 
+		
+		$response = $ses->send_email($this->from, $destination, $message, $options); 
+		$ok = $response->isOK(); 
+		if(!$ok)
+		{ 
+			$this->log('Error sending email from AWS SES: ' . $response->body->asXML(), 'debug'); 
+		}
+		
+		return $ok; 
+	}
+	
+	function verifyEmailAddress($email)
+	{
+		$ses = $this->getAmazonSES();
+		
+		$response = $ses->verify_email_address($email, $this->ses_options); 
+		$ok = $response->isOK(); 
+		if(!$ok)
+		{ 
+			$this->log('Error verify email address from AWS SES: ' . $response->body->asXML(), 'debug'); 
+		}
+		
+		return $ok;
+	}
+	
+	function deleteVerifiedEmailAddress($email)
+	{
+		$ses = $this->getAmazonSES();
+		
+		$response = $ses->delete_verified_email_address($email, $this->ses_options); 
+		$ok = $response->isOK(); 
+		if(!$ok)
+		{ 
+			$this->log('Error delete_verified_email_address from AWS SES: ' . $response->body->asXML(), 'debug'); 
+		}
+		
+		return $ok;
+	}
+	
+	function listVerifiedEmailAddresses()
+	{
+		$ses = $this->getAmazonSES();
+		
+		$response = $ses->list_verified_email_addresses($this->ses_options); 
+		$ok = $response->isOK(); 
+		if(!$ok)
+		{ 
+			$this->log('Error list_verified_email_addresses from AWS SES: ' . $response->body->asXML(), 'debug'); 
+		}
+		
+		return $response->body;
+	}
+	
+	function getSendQuota()
+	{
+		$ses = $this->getAmazonSES();
+		
+		$response = $ses->get_send_quota($this->ses_options); 
+		$ok = $response->isOK(); 
+		if(!$ok)
+		{ 
+			$this->log('Error get send quota from AWS SES: ' . $response->body->asXML(), 'debug'); 
+		}
+		
+		return $response->body;
+	}
+	
+	function getSendStatistics()
+	{
+		$ses = $this->getAmazonSES();
+		
+		$response = $ses->get_send_statistics($this->ses_options); 
+		$ok = $response->isOK(); 
+		if(!$ok)
+		{ 
+			$this->log('Error get send statistics from AWS SES: ' . $response->body->asXML(), 'debug'); 
+		}
+		
+		return $response->body;
+	}
+    
+}
+?>
